9923e8838be0a6e0b3f31e0112f33d5ccacf761e
n00bNn00b <reldewan9@gmail.com>
2022-05-19 17:51:28 +0600

    doctors portal initial setup with tailwind and daisyui

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Pages/About/About";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Navbar from "./Pages/Shared/Navbar/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
