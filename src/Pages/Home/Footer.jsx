d1547c7aeb1318fdf7ed1ac2793ca6150e5abd3e
n00bNn00b <reldewan9@gmail.com>
2022-05-22 00:33:43 +0600

    classNames modified

--- src/Pages/Home/Footer.jsx
+++ src/Pages/Home/Footer.jsx
@@ -8,27 +8,29 @@ const Footer = () => {
         background: ` url(${footer})`,
         backgroundSize: "cover",
       }}
-      class="footer p-10"
+      className="p-10"
     >
-      <div>
-        <span class="footer-title">Services</span>
-        <a class="link link-hover">Branding</a>
-        <a class="link link-hover">Design</a>
-        <a class="link link-hover">Marketing</a>
-        <a class="link link-hover">Advertisement</a>
-      </div>
-      <div>
-        <span class="footer-title">Company</span>
-        <a class="link link-hover">About us</a>
-        <a class="link link-hover">Contact</a>
-        <a class="link link-hover">Jobs</a>
-        <a class="link link-hover">Press kit</a>
-      </div>
-      <div>
-        <span class="footer-title">Legal</span>
-        <a class="link link-hover">Terms of use</a>
-        <a class="link link-hover">Privacy policy</a>
-        <a class="link link-hover">Cookie policy</a>
+      <div className="footer">
+        <div>
+          <span className="footer-title">Services</span>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </div>
       </div>
     </footer>
   );
--- src/Pages/Home/Services.jsx
+++ src/Pages/Home/Services.jsx
@@ -33,7 +33,7 @@ const Services = () => {
       </div>
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         {services.map((service) => (
-          <Service key={service.id} service={service} />
+          <Service key={service._id} service={service} />
         ))}
       </div>
     </div>
