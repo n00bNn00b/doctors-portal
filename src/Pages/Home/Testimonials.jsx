1d2253e1c19c20fa9c00569c84a15d94dd9c716c
n00bNn00b <reldewan9@gmail.com>
2022-05-21 23:45:30 +0600

    testimonials component has been added

--- src/Pages/Home/Testimonials.jsx
+++ src/Pages/Home/Testimonials.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 import quote from "../../assets/icons/quote.svg";
+import people1 from "../../assets/images/people1.png";
+import people2 from "../../assets/images/people2.png";
+import people3 from "../../assets/images/people3.png";
+import Review from "./Review";
 
 const Testimonials = () => {
+  const reviews = [
+    {
+      _id: 1,
+      name: "Winson Herry",
+      img: people1,
+      review: "Good",
+    },
+    {
+      _id: 2,
+      name: "Winson Herry",
+      img: people2,
+      review: "Good",
+    },
+    {
+      _id: 3,
+      name: "Winson Herry",
+      img: people3,
+      review: "Good",
+    },
+  ];
   return (
     <section className="my-28">
       <div className="flex justify-between">
@@ -13,7 +37,11 @@ const Testimonials = () => {
           <img className="w-24 lg:w-48" src={quote} alt="quote" />
         </div>
       </div>
-      <div></div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {reviews.map((review) => (
+          <Review key={review._id} review={review} />
+        ))}
+      </div>
     </section>
   );
 };
